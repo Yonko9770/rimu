9df88f736b8e78384163784f8374d5263d7a21ad
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-03-08 22:15:46 +0530

    Update cleaner.py

--- DestinyBot/modules/cleaner.py
+++ DestinyBot/modules/cleaner.py
@@ -222,7 +222,7 @@ __help__ = """
 ✮ /unignoreblue <word>*:* remove prevent auto cleaning of the command
 ✮ /listblue*:* list currently whitelisted commands
 
- *Following are Disasters only commands, admins cannot use these:*
+ *Following are Member only commands, admins cannot use these:*
 
 ✮ /gignoreblue <word>*:* globally ignorea bluetext cleaning of saved word across Destiny.
 ✮ /ungignoreblue <word>*:* remove said command from global cleaning list.
