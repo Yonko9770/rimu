3547a1bf1ed61008b9155f288ea65a446c94d1ac
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-03-10 11:55:53 +0530

    Update mongo.py

--- DestinyBot/mongo.py
+++ DestinyBot/mongo.py
@@ -10,14 +10,14 @@ from DestinyBot.confing import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "alucard"
+MONGO_DB = "Alucard"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["alucard"]
+db = client["Alucard"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
