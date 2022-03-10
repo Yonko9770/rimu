4177bba4e39ba746277e1c907e1979360e35ce07
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-03-10 21:41:54 +0530

    Update antiflood.py

--- DestinyBot/modules/antiflood.py
+++ DestinyBot/modules/antiflood.py
@@ -400,12 +400,12 @@ def __chat_settings__(chat_id, user_id):
 __help__ = """
 Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
- ✮ /flood: Get the current flood control setting
-✧ Admins only:
- ✮ /setflood <int/'no'/'off'>: enables or disables flood control
+ ✟ /flood: Get the current flood control setting
+➣ Admins only:
+ ✟ /setflood <int/'no'/'off'>: enables or disables flood control
  Example: /setflood 10
- ✮ /setfloodmode <ban/kick/mute/tban/tmute> <value>: Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
-✧ Note:
+ ✟ /setfloodmode <ban/kick/mute/tban/tmute> <value>: Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+➣ Note:
  • Value must be filled for tban and tmute!!
  It can be:
  5m = 5 minutes
@@ -414,7 +414,7 @@ Antiflood allows you to take action on users that send more than x messages in a
  1w = 1 week
 """
 
-__mod_name__ = "Anti-Flood"
+__mod_name__ = "✟ Anti-Flood ✟"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups,
