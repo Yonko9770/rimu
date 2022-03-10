f27d683d46f797b04970d5a0bf7e674768e448a3
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-03-10 13:05:57 +0530

    Update disasters.py

--- DestinyBot/modules/disasters.py
+++ DestinyBot/modules/disasters.py
@@ -644,49 +644,49 @@ Commands listed here only work for users with special
 access are mainly used for troubleshooting, debugging purposes.
 Group admins/group owners do not need these commands. 
  ╔ *List all special users:*
- ╠ ✮ `/protestines` or `/sudolist`*:* Lists all Dragon disasters
- ╠ ✮ `/catholics` or `/supportlist`*:* Lists all Demon disasters
- ╠ ✮ `/butlers` or `/tigerlist`*:* Lists all Tigers disasters
- ╠ ✮ `/ghouls` or `/whitelist`*:* Lists all Wolf disasters
- ╠ ✮ `/hell_mem` or `/devlist`*:* Lists all Dev disastors
- ╠ ✮ `/addmusicart` or `/addsudo`*:* Adds a user to Dragon
- ╠ ✮ `/addd2slayer` or `/addsupport`*:* Adds a user to Demon
- ╠ ✮ `/adddefender` or `/addtiger`*:* Adds a user to Tiger
- ╚ ✮ `/addmelcr` or `/addwolf`*:* Adds a user to Wolf 
+ ╠ ✟ `/protestines` or `/sudolist`*:* Lists all Dragon disasters
+ ╠ ✟ `/catholics` or `/supportlist`*:* Lists all Demon disasters
+ ╠ ✟ `/butlers` or `/tigerlist`*:* Lists all Tigers disasters
+ ╠ ✟ `/ghouls` or `/whitelist`*:* Lists all Wolf disasters
+ ╠ ✟ `/hell_mems` or `/devlist`*:* Lists all Dev disastors
+ ╠ ✟ `/addprotest` or `/addsudo`*:* Adds a user to Dragon
+ ╠ ✟ `/addcatholic` or `/addsupport`*:* Adds a user to Demon
+ ╠ ✟ `/addbutler` or `/addtiger`*:* Adds a user to Tiger
+ ╚ ✟ `/addghoul` or `/addwolf`*:* Adds a user to Wolf 
  ╔ *Ping:*
- ╠ ✮ `/ping`*:* gets ping time of bot to telegram server
- ╚ ✮ `/pingall`*:* gets all listed ping times
+ ╠ ✟ `/ping`*:* gets ping time of bot to telegram server
+ ╚ ✟ `/pingall`*:* gets all listed ping times
  ╔ *Broadcast: (Bot owner only)*
  ╠  *Note:* This supports basic markdown
- ╠ ✮ `/broadcastall`*:* Broadcasts everywhere
- ╠ ✮ `/broadcastusers`*:* Broadcasts too all users
- ╚ ✮ `/broadcastgroups`*:* Broadcasts too all groups
+ ╠ ✟ `/broadcastall`*:* Broadcasts everywhere
+ ╠ ✟ `/broadcastusers`*:* Broadcasts too all users
+ ╚ ✟ `/broadcastgroups`*:* Broadcasts too all groups
  ╔ *Groups Info:*
- ╠ ✮ `/groups`*:* List the groups with Name, ID, members
+ ╠ ✟ `/groups`*:* List the groups with Name, ID, members
  ╠    count as a txt
- ╠ ✮ `/leave <ID>`*:* Leave the group, ID must have hyphen
- ╠ ✮ `/stats`*:* Shows overall bot stats
- ╠ ✮ `/getchats`*:* Gets a list of group names the user has 
+ ╠ ✟ `/leave <ID>`*:* Leave the group, ID must have hyphen
+ ╠ ✟ `/stats`*:* Shows overall bot stats
+ ╠ ✟ `/getchats`*:* Gets a list of group names the user has 
  ╠    been seen in. Bot owner only
- ╚ ✮ `/ginfo username/link/ID`*:* Pulls info panel for 
+ ╚ ✟ `/ginfo username/link/ID`*:* Pulls info panel for 
        entire group
  ╔ *Access control:* 
- ╠ ✮ `/ignore`*:* Blacklists a user from 
+ ╠ ✟ `/ignore`*:* Blacklists a user from 
  ╠     using the bot entirely
- ╠ ✮ `/notice`*:* Removes user from blacklist
- ╚ ✮ `/ignoredlist`*:* Lists ignored users
+ ╠ ✟ `/notice`*:* Removes user from blacklist
+ ╚ ✟ `/ignoredlist`*:* Lists ignored users
  ╔ *Windows self hosted only:*
- ╠ ✮ `/reboot`*:* Restarts the bots service
- ╚ ✮ `/gitpull`*:* Pulls the repo and then restarts the
+ ╠ ✟ `/reboot`*:* Restarts the bots service
+ ╚ ✟ `/gitpull`*:* Pulls the repo and then restarts the
        bots service
  ╔ *Chatbot:* 
- ╚ ✮ `/listaichats`*:* Lists the chats the chatmode
+ ╚ ✟ `/listaichats`*:* Lists the chats the chatmode
        is enabled in
  ╔ *Global Bans:*
- ╠ ✮ `/gban <id> <reason>`*:* Gbans the user,
+ ╠ ✟ `/gban <id> <reason>`*:* Gbans the user,
        works by reply too
- ╠ ✮ `/ungban`*:* Ungbans the user, same usage as gban
- ╚ ✮ `/gbanlist`*:* Outputs a list of gbanned users
+ ╠ ✟ `/ungban`*:* Ungbans the user, same usage as gban
+ ╚ ✟ `/gbanlist`*:* Outputs a list of gbanned users
 Visit @HellSing_Organisation for more information.
 """
 
