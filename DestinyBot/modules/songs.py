51589f23bda662f5a132e5164d99d3722cd84fb8
FreakMask <98258230+FreakMask@users.noreply.github.com>
2022-03-11 23:24:45 +0530

    Update songs.py

--- DestinyBot/modules/songs.py
+++ DestinyBot/modules/songs.py
@@ -2,12 +2,12 @@ __help__ = """
 
 *Music Modules:*
 
-✮ /video or /vsong (query): download video from youtube
+✟ /video or /vsong (query): Alucard will download the video from youtube
 
-✮ /music or /song (query): download song from yt servers. (API BASED)
+✟ /music or /song (query): Alucard will download the song from yt servers. (API BASED)
 
-✮ /lyrics (song name) : This plugin searches for song lyrics with song name.
+✟ /lyrics (song name) : This plugin in Alucard searches for song lyrics with song name.
 
 """
 
-__mod_name__ = "Songs"
+__mod_name__ = "✟ Songs ✟"
