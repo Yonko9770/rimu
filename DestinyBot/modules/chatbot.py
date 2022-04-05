fa46db0b8b5d794f4c6ac79ce3db7f1306749f9d
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-05 18:23:36 +0530

    Update chatbot.py

--- DestinyBot/modules/chatbot.py
+++ DestinyBot/modules/chatbot.py
@@ -116,7 +116,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://kukiapi.xyz/api/apikey=KUKItg111XlOZ/yuzuki/moezill/message='+Message)
+        kukiurl = requests.get('http://api.open-notify.org/iss-now.json/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
