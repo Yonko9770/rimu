2b0f858ad190ebbba13e735b95cc733f4dfcd04f
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-11 19:57:33 +0530

    Create pokedex_help.py

---
+++ DestinyBot/modules/pokedex_help.py
@@ -0,0 +1,10 @@
+__mod_name__ = "✟ Pokedex ✟"
+
+_help_ = """
+
+*Pokedex*
+
+✟ /pokedex <name of pokemon> It gathers information about the Pokemon, if the name is given correctly.
+
+Note: Spell the Pokemon name properly or else won't gonna work.
+"""
