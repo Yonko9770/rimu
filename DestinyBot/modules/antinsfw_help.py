837da5bf607f25f0bb5a634614adfc93aaf73fad
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-11 20:26:11 +0530

    Create antinsfw_help.py

---
+++ DestinyBot/modules/antinsfw_help.py
@@ -0,0 +1,14 @@
+__mod_name__ ="✟ Anti-NSFW ✟"
+
+__help__ ="""
+
+
+*Anti-NSFW:*
+
+ ✟ /antinsfw [on/off]*:* To enable/disable antinsfw option in your chat.
+
+ ✟ /nsfwscan*:* Reply to image/sticker/gif/video to check if it's a NSFW content or not.
+
+ ✟ /profanity [on/off]*:* To restrict the use of some profanity/slang words.
+ 
+ """
