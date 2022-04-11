f335461877548f481539d1ec920f6529ea7cd059
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-11 19:49:13 +0530

    Create memes_help.py

---
+++ DestinyBot/modules/memes_help.py
@@ -0,0 +1,13 @@
+__mod_name__ ="✟ Meme ✟"
+
+__help__ ="""
+
+*Memes*
+
+Note: 
+
+• This module utilizes meme-api.heroku.com, which uses reddit as a source.
+
+ ✟ /meme: To get random wholesome memes from subreddit r/wholesomememes.
+ 
+ """
