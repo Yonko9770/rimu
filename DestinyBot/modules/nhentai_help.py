ad232d3817d9da4971e6ee8df02be775d21acd1c
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-11 19:32:52 +0530

    Create nhentai_help.py

---
+++ DestinyBot/modules/nhentai_help.py
@@ -0,0 +1,17 @@
+__mod_name__ = "✟ Nhentai ✟"
+
+__help__ = """
+
+*Nhentai / Doujins*
+
+✟ /nhentai <doujin code>*:* To get a doujin for the provided code.
+
+✟ /nhentai random*:* It provides random doujins irrespective or type and language.
+
+Note:
+
+1. This will only work if you are in nsfw mode either in group or in private chat. do /addnsfw to enable nsfw mode before using it.
+
+2. If you need doujin codes, you can visit [Hentai Hostel](https://t.me/+EO5yc0ryc0FkZDU1).
+
+"""
