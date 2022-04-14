8ee47f862c8e13889832e5e4c3e54b28b1b2678f
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-14 12:45:42 +0530

    Create request_help.py

---
+++ DestinyBot/modules/request_help.py
@@ -0,0 +1,10 @@
+__mod_name__ ="✟ Request ✟"
+
+__help__ ="""
+
+✟ /request <your request>: Sends a request message mentioned by the user directly to the HellSing Organisation.
+All requests gets mentioned in the [HellSing Request Centre](https://t.me/+mI7_0HdJPGhmNzI1) Channel that is private.
+
+*NOTE:*
+It will also collect user's Information, especially there telegram id. So think twice before spamming it.
+"""
