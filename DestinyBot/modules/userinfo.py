df5c2543ddc39e6560be7a0dca4d606b9afd10be
Ғʀᴇᴀ̄ᴋ <98258230+FreakMask@users.noreply.github.com>
2022-04-15 01:13:49 +0530

    Update userinfo.py

--- DestinyBot/modules/userinfo.py
+++ DestinyBot/modules/userinfo.py
@@ -122,7 +122,7 @@ def hpmanager(user):
 
 def make_bar(per):
     done = min(round(per / 10), 10)
-    return "■" * done + "□" * (10 - done)
+    return "✟" * done + "†" * (10 - done)
 
 
 
@@ -249,7 +249,7 @@ def info(update: Update, context: CallbackContext):
     rep = message.reply_text("<code>Accessing info from HellSing Organisation...</code>", parse_mode=ParseMode.HTML)
 
     text = (
-        f"╒═══<b>「Database of{html.escape(user.first_name)}:」</b>\n"
+        f"╒═══<b>「Database of {mention_html(user.id,first_name)}:」</b>\n"
         f"✟ ID: <code>{user.id}</code>\n"
         f"✟ First Name: {html.escape(user.first_name)}"
     )
@@ -279,7 +279,7 @@ def info(update: Update, context: CallbackContext):
                     text += _stext.format("Admin")
     if user_id not in [bot.id, 777000, 1087968824]:
         userhp = hpmanager(user)
-        text += f"\n\n<b>Health:</b> <code>{userhp['earnedhp']}/{userhp['totalhp']}</code>\n[<i>{make_bar(int(userhp['percentage']))} </i>{userhp['percentage']}%]"
+        text += f"\n\n✟ <b>Health:</b> <code>{userhp['earnedhp']}/{userhp['totalhp']}</code>\n[<i>{make_bar(int(userhp['percentage']))} </i>{userhp['percentage']}%]"
 
     try:
         spamwtc = sw.get_ban(int(user.id))
